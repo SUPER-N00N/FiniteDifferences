723f32ea3293c223b6bb71361d2c0958ea7151af
SUPER N00N <snaske@77k.eu>
2016-11-01 17:35:48 +0100

    yoo

--- tree.h
+++ tree.h
@@ -1,6 +1,7 @@
 /**
  * sn00n@77k.eu-
  */
+// 
 #ifndef TREE_H
 #define TREE_H
 
@@ -21,6 +22,47 @@ typedef boost::multiprecision::uint256_t uint256_t;
 typedef unsigned __int128 uint128_t;
 typedef __m256 uint256_t;
 #endif
+
+enum class ArchType: short
+{
+    GENERIC,
+    AMD64,
+    AARCH64,
+};
+
+enum class LinkType: bool
+{
+    Single,
+    Double,
+};
+
+enum class AccessScheme: bool
+{
+    Pointer,
+    Index,
+};
+
+
+struct IF
+{
+        typedef _then RET;
+};
+
+template< class _then, class _else >
+struct IF< false, _then, _else >
+{
+        typedef _else RET;
+};
+
+template< int N > struct ifact{ enum { eval = N * ifact< N - 1>::eval }; };
+template <> struct ifact< 0 > { enum { eval = 1 }; };
+template< int N, int K >
+struct nchoosek{ 
+    enum { 
+        eval = ifact< N >::eval / ( ifact< K >::eval *  ifact< N - K >::eval)
+    };
+};
+
 
 
 #endif
