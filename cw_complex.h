1e394770985f38d3c07f2743134175ccad013299
SUPER N00N <snaske@77k.eu>
2016-12-20 12:45:05 +0100

    abstract oriented CW Cell stuff added

--- cw_complex.h
+++ cw_complex.h
@@ -19,6 +19,52 @@ template< int _Dim, LinkType _LType, AccessScheme _AScheme,
     template< class U, class V > class _Containment,
     template< class U > class _Allocator, class _Space >
     struct AbstractOrientedCWCell: _Space {};
+//kolmogorov classes -> separation -> stratification
+
+//why d + 1 -> -\f$ {d+1 \choose d} = d+1\f$  
+//cartan filtration ends at d = -1
+
+template< int _Dim,  template< class U, class V > class _Containment,
+    template< class U > class _Allocator,  class _Space >
+    struct AbstractOrientedCWCell< _Dim, LinkType::Single, AccessScheme::Index,
+    _Containment, _Allocator, _Space >: _Space
+{
+    enum {d = _Dim};
+    ptrdiff_t upper, opponent,
+              next;
+    //ptrdiff_t lower[_Dim + 1]; 
+    ptrdiff_t lower;
+    AbstractOrientedCWCell()
+    {
+        upper = -1;
+        opponent = -1;
+        next = -1;
+        lower = -1;
+    };
+    AbstractOrientedCWCell(const AbstractOrientedCWCell &s): _Space(s)
+    {
+        upper = s.upper;
+        opponent = s.opponent;
+        //opponext = s.opponext;
+        next = s.next;
+        lower = s.lower;
+    }
+};
+
+///terminate the recursion of the filtration in the empty Abstract CW Cell with
+//dimension -1
+template< template< class U, class V > class _Containment,
+    template< class U > class _Allocator,
+    class _Space >
+    struct AbstractOrientedCWCell< -1, LinkType::Single,
+    AccessScheme::Index, _Containment,
+    _Allocator, _Space >: _Space
+{
+    enum { d = -1, };
+    AbstractOrientedCWCell(){};
+    AbstractOrientedCWCell(AbstractOrientedCWCell &s){};
+};
+
 
 
 #endif
