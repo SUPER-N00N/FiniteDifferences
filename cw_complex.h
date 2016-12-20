e7e8ea10a6115ceed2cf8d6198456bdb36bc0b2d
SUPER N00N <snaske@77k.eu>
2016-12-20 12:06:55 +0100

    yo

--- cw_complex.h
+++ cw_complex.h
@@ -4,4 +4,21 @@
 #ifndef CW_COMPLEX_H
 #define CW_COMPLEX_H
 
+#include "algebra.h"
+
+template< int _Dim, LinkType _LType, AccessScheme _AScheme,
+    template< class U , class V > class Containment , template< class U >
+    class Allocator, class _Space > class AbstractOrientedCWComplex{};
+template< typename _ASD > struct AbstractOrientedCWComplexTopologyTrait{};
+template< typename _ASD > struct
+AbstractOrientedCWComplexTopologicalOperations{};
+template< typename _Iterator >
+struct AbstractOrientedCWComplexIteratorFunctionTrait{};
+template< typename _ASD > struct AbstractOrientedCWComplexIterator{};
+template< int _Dim, LinkType _LType, AccessScheme _AScheme,
+    template< class U, class V > class _Containment,
+    template< class U > class _Allocator, class _Space >
+    struct AbstractOrientedCWCell: _Space {};
+
+
 #endif
