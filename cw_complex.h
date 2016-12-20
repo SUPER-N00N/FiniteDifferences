2353a34283498c25ad6b78846fe099725190b1d0
SUPER N00N <snaske@77k.eu>
2016-12-20 20:13:44 +0100

    foo

--- cw_complex.h
+++ cw_complex.h
@@ -66,5 +66,71 @@ template< template< class U, class V > class _Containment,
 };
 
 
+template< int _Dim,  template< class U, class V > class _Containment, 
+    template< class U > class _Allocator > 
+    struct AbstractOrientedCWCell< _Dim, LinkType::Single, 
+    AccessScheme::Index, _Containment, _Allocator, Set< _Dim > >: Set < _Dim > 
+{
+    enum {d = _Dim};
+    ptrdiff_t upper, opponent,
+              //opponext,
+              next;
+    //ptrdiff_t lower[_Dim + 1];
+    ptrdiff_t lower;
+    AbstractOrientedCWCell(){};
+    AbstractOrientedCWCellAbstractOrientedCWCell &s){};
+};
+
+template< template< class U, class V > class _Containment,
+    template< class U > class _Allocator >
+struct AbstractOrientedCWCell< -1, LinkType::Single, AccessScheme::Index, _Containment,
+    _Allocator, Set< -1 > >: Set< -1 >
+{
+    enum { d = -1, };
+    AbstractOrientedCWCell(){};
+    AbstractOrientedCWCell(AbstractOrientedCWCell &s){};
+};
+
+template< int D, class CWC > struct makeCWCell
+{
+    static inline void make(CWC &c)
+    {
+
+    }
+
+};
+
+template< int D, class _CWC > struct ContainerFiller
+{
+    static inline void fill(_CWC& c)
+    {   
+        s.cell_containers[D] = new typename _SC::template Containment<
+            typename _CWC::template CWCell< D >, typename _SC::template Allocator<
+            typename _CWC::template CWCell< D > > >;
+        ContainerFiller< D - 1, _SC >::fill(s);
+    };
+};
+
+template< class _CWC >
+struct ContainerFiller< -1, _CWC >{ static inline void fill(_CWC&){}};
+
+
+template< int _D, class _IT, class  _CWC > struct IterFiller
+{
+    static inline void fill(_IT* i)
+    {
+        typedef  AbstractOrientedCWCell< _D, LinkType::Single,
+                 AccessScheme::Index, _SC::template Containment,
+                 _SC::template Allocator,
+                 typename _CWC::Space > CWCell;
+        i->iterdata[_D ] = new CWCell;
+        //void* foo = new CWCell;
+        IterFiller< _D - 1, _IT, _CWC >::fill(i);
+    
+};
+
+template< class _IT, class _CWC > struct IterFiller< -1, _IT, _CWC >{
+    static inline void fill(_IT* i){}
+};
 
 #endif
